0bd2d3b4d17452b932de0c9862eaa0c4fd7d55d1
Abdellah Hariti <haritiabdellah@gmail.com>
2024-05-31 12:56:49 +0100

    remove unecessary override from eslintrc

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -11,14 +11,4 @@ module.exports = {
   rules: {
     'import/no-nodejs-modules': 'off',
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        // Reach is vendored into gatsby. They have their own webpack
-        // resolution for it which this eslint plugin can't seem to detect
-        'import/no-unresolved': ['error', {ignore: ['@reach']}],
-      },
-    },
-  ],
 };
